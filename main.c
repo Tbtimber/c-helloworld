7096ff5d6ab0bb80b0396ee1cfc5c1851804e20a
mdelpierre <matthieu.delpierre@altran.com>
2019-02-06 21:36:01 +0100

    Final sonar test threshold

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
-
+/*
+Main program function call for hello world
+*/
 int main(int argc, char const *argv[]) {
   int unused[5] = {0,1,2,3,4};
   printf("Hello world ! %c \n", argv[0]);
