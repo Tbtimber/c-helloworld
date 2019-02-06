9635adc798d9afab4e80714cafc03dc60c7f49fb
mdelpierre <matthieu.delpierre@altran.com>
2019-02-06 21:55:50 +0100

    added src folder

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 Main program function call for hello world
 */
 int main(int argc, char const *argv[]) {
-  int unused[5] = {0,1,2,3,4};
-  printf("Hello world ! %c \n", argv[0]);
+  printf("Hello world !\n");
   return 0;
 }
