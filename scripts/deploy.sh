d7e33a626af890044d9dcae388d10e4a5839619d
mdelpierre <matthieu.delpierre@altran.com>
2019-02-24 14:57:07 +0100

    ak

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,13 @@ echo '=====================Deployment=============================='
 
 #On failure return with null
 echo '=======================Authentication with remoteIt==========='
-remoteItAuthToken='curl -X POST \
+remoteItAuthToken=curl -X POST \
      -H "developerkey":"$REMOTEIT_DEVELOPER_KEY" \
      -d '{"username":"'$REMOTEIT_USERNAME'","password":"'$REMOTEIT_PASSWORD'"}' \
      https://api.remot3.it/apv/v27/user/login |\
-     jq ".auth_token"'
-echo '\n=============Authentication over========================\n'
-echo '\n=======================Retrieving devices list==============='
+     jq ".auth_token"
+echo '=============Authentication over========================'
+echo '=======================Retrieving devices list==============='
 
 curl -H "token:$remoteItAuthToken" \
      -H "developerkey:$REMOTEIT_DEVELOPER_KEY" \
