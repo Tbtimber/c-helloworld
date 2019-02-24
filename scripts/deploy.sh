14b876bce2b32e0ad5382d15403ec60b47f55ac3
mdelpierre <matthieu.delpierre@altran.com>
2019-02-24 13:21:27 +0100

    see log in travis console

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
+echo '=====================Deployment=============================='
 #Authenticate with https://api.remot3.it/apv/v27/user/login
 #Param is -H dev KEY (passed as env variable from Travis CI)
 #Param is -B username (passed as env variable from Travis CI)
 #PAram is -B Password (passed as env variable from Travis CI)
 
 #On failure return with null
+echo '=======================Authentication with remoteIt==========='
 curl -X POST \
      -H "developerkey":"$REMOTEIT_DEVELOPER_KEY" \
      -d '{"username":"'$REMOTEIT_USERNAME'","password":"'$REMOTEIT_PASSWORD'"}' \
