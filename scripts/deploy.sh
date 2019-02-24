b4f4e1441d4f67d3e978fd9609dcf3af9f467507
mdelpierre <matthieu.delpierre@altran.com>
2019-02-24 13:46:04 +0100

    add quote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ authCall= curl -X POST \
      -H "developerkey":"$REMOTEIT_DEVELOPER_KEY" \
      -d '{"username":"'$REMOTEIT_USERNAME'","password":"'$REMOTEIT_PASSWORD'"}' \
      https://api.remot3.it/apv/v27/user/login
-echo $authCall
-echo $authCall | jq '.'
+echo '$authCall'
+echo '$authCall | jq \'.\''
 
 #After Authenticate :
