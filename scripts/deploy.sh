d45485ba23c328912874167b7cb53d22646fe4a0
mdelpierre <matthieu.delpierre@altran.com>
2019-02-24 13:29:05 +0100

    added jq to script deploysh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,11 @@ echo '=====================Deployment=============================='
 
 #On failure return with null
 echo '=======================Authentication with remoteIt==========='
-curl -X POST \
+authCall = curl -X POST \
      -H "developerkey":"$REMOTEIT_DEVELOPER_KEY" \
      -d '{"username":"'$REMOTEIT_USERNAME'","password":"'$REMOTEIT_PASSWORD'"}' \
      https://api.remot3.it/apv/v27/user/login
-
+echo $authCall
+echo $authCall | jq '.'
 
 #After Authenticate :
