64743d3a1f4b6797f08f44975499af0b34c0ae59
mdelpierre <matthieu.delpierre@altran.com>
2019-02-24 13:07:27 +0100

    Test script Auth

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#Authenticate with https://api.remot3.it/apv/v27/user/login
+#Param is -H dev KEY (passed as env variable from Travis CI)
+#Param is -B username (passed as env variable from Travis CI)
+#PAram is -B Password (passed as env variable from Travis CI)
+
+#On failure return with null
+curl -X POST \
+     -H "developerkey":"$REMOTEIT_DEVELOPER_KEY" \
+     -d '{"username":"'$REMOTEIT_USERNAME'","password":"'$REMOTEIT_PASSWORD'"}' \
+     https://api.remot3.it/apv/v27/user/login
+
+
+#After Authenticate :
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-/*
-Main program function call for hello world
-*/
-int main(int argc, char const *argv[]) {
-  printf("Hello world !\n");
-  return 0;
-}
